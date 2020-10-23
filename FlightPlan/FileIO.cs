e6f7642afd10e49aaf4582527cec7a09ce4cd3e8
Jonathan Bayer <linuxgurugamer@gmail.com>
2020-10-23 15:09:41 -0400

    Some optimization (removed unnecessary loops)  Added mode to use or not use the UICore mode

---
+++ FlightPlan/FileIO.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommNet;
+
+namespace FlightPlan_NS
+{
+    public partial class FlightPlan
+    {
+        const string FLIGHTPLAN = "FlightPlan";
+        const string DIR = "GameData/FlightPlan/PluginData";
+        const string FILE = DIR + "/FlightPlan.cfg";
+        void SaveData()
+        {
+            ConfigNode filenode = new ConfigNode();
+            ConfigNode node = new ConfigNode(FLIGHTPLAN);
+            node.AddValue("uiCoreActive", uiCoreActive);
+            filenode.AddNode(node);
+            if (!Directory.Exists(DIR))
+                Directory.CreateDirectory(DIR);
+            filenode.Save(FILE);
+        }
+
+        void LoadData()
+        {
+            if (File.Exists(FILE))
+            {
+                ConfigNode filenode = ConfigNode.Load(FILE);
+                ConfigNode node = null;
+                if (filenode.TryGetNode(FLIGHTPLAN, ref node))
+                {
+                    if (node.TryGetValue("uiCoreActive", ref uiCoreActive))
+                        oldUiCoreActive = uiCoreActive;
+                }
+            }
+        }
+    }
+}
--- FlightPlan/ToolbarRegistration.cs
+++ FlightPlan/ToolbarRegistration.cs
@@ -10,5 +10,21 @@ namespace FlightPlan_NS
         {
             ToolbarControl.RegisterMod(FlightPlan.MODID, FlightPlan.MODNAME);
         }
+        void OnGUI()
+        {
+            if (FlightPlan.guiLabelStyle == null)
+            {
+                FlightPlan.guiLabelStyle = new GUIStyle(GUI.skin.label);
+                FlightPlan.guiWhiteLabelStyle = new GUIStyle(GUI.skin.label);
+                FlightPlan.guiBlueLabelStyle = new GUIStyle(GUI.skin.label);
+                FlightPlan.guiWhiteLabelStyle.normal.textColor = Color.white;
+                // Color values are in the range 0.0 - 1.0, not 0 - 255.
+                FlightPlan.guiBlueLabelStyle.normal.textColor = new Color(172f/255f, 172f/255f, 243f/255f);
+
+                FlightPlan.guiButtonStyle = new GUIStyle(GUI.skin.button);                
+                FlightPlan.guiGreenButtonStyle = new GUIStyle(GUI.skin.button);
+                FlightPlan.guiGreenButtonStyle.normal.textColor = Color.green;
+            }
+        }
     }
 }
